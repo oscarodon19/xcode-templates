edc8ca57010f4530670c913c7408befd69dcfcbc
Oscar Odon <oscar.odon@pomelo.la>
2023-06-23 13:11:35 +0200

    Script deletes unused files on Templates folder

--- DownloadCustomTemplates.sh
+++ DownloadCustomTemplates.sh
@@ -1,3 +1,7 @@
+MAGENTA=`tput setaf 5`
+GREEN=`tput setaf 2`
+RESET=`tput sgr0`
+
 USER_HOME=$(echo ~${SUDO_USER})
 
 echo "home it is : $USER_HOME"
@@ -9,27 +13,35 @@ TEMPLATES_PATH=${USER_HOME}/Library/Developer/Xcode/Templates
 FILE_TEMPLATES_PATH="${TEMPLATES_PATH}/File Templates"
 
 if [[ ! -e $TEMPLATES_PATH ]]; then
-    echo "Creating Templates directory $TEMPLATES_PATH"
+    echo "${MAGENTA}Creating Templates directory $TEMPLATES_PATH ${RESET}"
     mkdir $TEMPLATES_PATH
+    cd $TEMPLATES_PATH
 fi
 
-if [[ ! -e $FILE_TEMPLATES_PATH ]]; then
-    echo "Creating File Templates directory $FILE_TEMPLATES_PATH"
-    mkdir -p "${FILE_TEMPLATES_PATH}"
-fi
-
-echo "Copying the templates into the directory $FILE_TEMPLATES_PATH"
-cd "$FILE_TEMPLATES_PATH"
-SOURCE_FILE="${FILE_TEMPLATES_PATH}/xcode-templates"
+echo "${GREEN}Copying the templates into the directory $TEMPLATES_PATH ${RESET}"
+cd "$TEMPLATES_PATH"
+SOURCE_FILE="${TEMPLATES_PATH}/xcode-templates"
 
 if [[ ! -e $SOURCE_FILE ]]; then
-  echo "Cloning the directory "
-  git clone git@github.com:oscarodon19/xcode-templates.git
+    echo "Cloning the directory "
+    git clone git@github.com:oscarodon19/xcode-templates.git
 else
     cd "$SOURCE_FILE"
     echo "updating the directory "
     git pull
 fi
 
+cd $SOURCE_FILE
+if [ -f "DownloadCustomTemplates.sh" ]; then
+    rm -fr DownloadCustomTemplates.sh
+fi
 
+if [ -f ".gitignore" ]; then
+    rm -fr .gitignore
+fi
 
+if [ -f "README.md" ]; then
+    rm -fr README.md
+fi
+
+rm -fr .git
